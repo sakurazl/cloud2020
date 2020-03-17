0e411bfe440424d6f1ef695a32371887862ccd36
zl <1315267449@qq.com>
2020-03-17 22:35:29 +0800

    修改错误

--- cloud-config-center-3344/src/main/resources/bus-refresh.cmd
+++ cloud-config-center-3344/src/main/resources/bus-refresh.cmd
@@ -1,5 +1,5 @@
 # 全部通知
-curl +X POST "http://localhost:3344/actuator/bus-refresh"
+curl -X POST "http://localhost:3344/actuator/bus-refresh"
 
 # 公式 http://配置中心ip:配置中心端口/actuator/bus-refresh/{destination}
 # destination = 微服务名称：端口号
