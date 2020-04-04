82548f978e986a1cd52ae810388c90fa9407a2fb
zl <1315267449@qq.com>
2020-04-04 21:07:34 +0800

    新增springcloud alibaba nacos作为服务注册及配置中心

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/springcloud/controller/OrderController.java
@@ -0,0 +1,22 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderController {
+    @Resource
+    private RestTemplate restTemplate;
+    @Value("${service-url.nacos-payment-service}")
+    private String serverUrl;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Integer id){
+        return restTemplate.getForObject(serverUrl + "/payment/nacos/" + id, String.class);
+    }
+}
